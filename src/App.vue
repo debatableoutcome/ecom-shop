30a62939faec22c2b1e8321e3564c64d7044e6ec
debatableoutcome <dustypie@yandex.ru>
2023-10-20 15:11:22 +0300

    добавила галерею и подключила api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <Header />
-  <FilterPanel />
+  <main>
+    <Header />
+    <div class="lower-container">
+      <FilterPanel />
+      <ProductScreen />
+    </div>
+  </main>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import FilterPanel from "./components/FilterPanel.vue";
+import ProductScreen from "./components/ProductScreen.vue";
 
 export default {
   name: "App",
   components: {
     Header,
     FilterPanel,
+    ProductScreen,
   },
 };
 </script>
@@ -24,5 +31,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: green;
+}
+.lower-container {
+  background: hotpink;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
 }
 </style>
---
+++ src/components/ProductTile.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="product-card">
+    <img
+      :src="`https://picsum.photos/640/480?random=${product.id}`"
+      alt="Product Image"
+      class="product-card__image"
+    />
+
+    <div class="product-card__content">
+      <h2 class="product-card__title">{{ product.name }}</h2>
+      <p class="product-card__description">{{ shortDescription }}</p>
+      <p class="product-card__price">{{ product.price }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    shortDescription() {
+      return this.product.description.length > 200
+        ? this.product.description.substring(0, 197) + "..."
+        : this.product.description;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.product-card {
+  width: 250px;
+  border: 1px solid #e1e1e1;
+  background-color: white;
+  overflow: hidden;
+}
+
+.product-card__image {
+  width: 100%;
+  height: auto;
+  display: block;
+}
+
+.product-card__content {
+  padding: 15px;
+}
+
+.product-card__title {
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+
+.product-card__description {
+  font-size: 14px;
+  color: #777;
+  margin-bottom: 10px;
+}
+
+.product-card__price {
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
